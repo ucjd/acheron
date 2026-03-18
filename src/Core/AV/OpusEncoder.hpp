21b2f0d25f34d2cdcc2347fc187fc7037af903ef
ucjd <134995775+ucjd@users.noreply.github.com>
2026-03-17 20:51:34 -0500

    here too

--- src/Core/AV/OpusEncoder.hpp
+++ src/Core/AV/OpusEncoder.hpp
@@ -1,39 +1,30 @@
 #pragma once
-
 #include <QByteArray>
-
 #include <opus.h>
-
 namespace Acheron {
 namespace Core {
 namespace AV {
-
 class OpusEncoder
 {
 public:
     OpusEncoder();
     ~OpusEncoder();
-
     OpusEncoder(const OpusEncoder &) = delete;
     OpusEncoder &operator=(const OpusEncoder &) = delete;
-
     bool init(int sampleRate, int channels);
-
     QByteArray encode(const QByteArray &pcmFrame);
-
     void setBitrate(int bitrate);
     void setComplexity(int complexity);
     void setSignalType(int signalType);
     void setFec(bool enabled);
     void setDtx(bool enabled);
     void setPacketLossPercent(int percent);
-
+    void setVbr(bool enabled);
 private:
     ::OpusEncoder *encoder = nullptr;
     int frameSamples = 0;
     int frameChannels = 0;
 };
-
 } // namespace AV
 } // namespace Core
 } // namespace Acheron
