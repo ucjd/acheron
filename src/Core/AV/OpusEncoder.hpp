d5d5afcb185f976861968d97dfe8a4457b7eb90b
ucjd <134995775+ucjd@users.noreply.github.com>
2026-03-21 01:56:26 -0500

    depth

--- src/Core/AV/OpusEncoder.hpp
+++ src/Core/AV/OpusEncoder.hpp
@@ -1,11 +1,9 @@
 #pragma once
 #include <QByteArray>
 #include <opus.h>
-
 namespace Acheron {
 namespace Core {
 namespace AV {
-
 class OpusEncoder
 {
 public:
@@ -13,26 +11,23 @@ public:
     ~OpusEncoder();
     OpusEncoder(const OpusEncoder &) = delete;
     OpusEncoder &operator=(const OpusEncoder &) = delete;
-
     bool init(int sampleRate, int channels);
     QByteArray encode(const QByteArray &pcmFrame);
-
     void setBitrate(int bitrate);
     void setComplexity(int complexity);
     void setSignalType(int signalType);
-    void setBandwidth(int bandwidth);
+    void setMaxBandwidth(int bandwidth);
+    void setLsbDepth(int depth);
     void setFec(bool enabled);
     void setDtx(bool enabled);
     void setPacketLossPercent(int percent);
     void setVbr(bool enabled);
     void setVbrConstraint(bool enabled);
-
 private:
     ::OpusEncoder *encoder = nullptr;
     int frameSamples = 0;
     int frameChannels = 0;
 };
-
 } // namespace AV
 } // namespace Core
 } // namespace Acheron
