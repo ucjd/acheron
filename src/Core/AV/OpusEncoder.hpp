6d09a979741abfb5c2c5cdfa18d6c2e38185dd04
ucjd <134995775+ucjd@users.noreply.github.com>
2026-03-20 22:46:07 -0500

    add vbr constraint

--- src/Core/AV/OpusEncoder.hpp
+++ src/Core/AV/OpusEncoder.hpp
@@ -1,18 +1,23 @@
 #pragma once
 #include <QByteArray>
 #include <opus.h>
+
 namespace Acheron {
 namespace Core {
 namespace AV {
+
 class OpusEncoder
 {
 public:
     OpusEncoder();
     ~OpusEncoder();
+
     OpusEncoder(const OpusEncoder &) = delete;
     OpusEncoder &operator=(const OpusEncoder &) = delete;
+
     bool init(int sampleRate, int channels);
     QByteArray encode(const QByteArray &pcmFrame);
+
     void setBitrate(int bitrate);
     void setComplexity(int complexity);
     void setSignalType(int signalType);
@@ -20,11 +25,14 @@ public:
     void setDtx(bool enabled);
     void setPacketLossPercent(int percent);
     void setVbr(bool enabled);
+    void setVbrConstraint(bool enabled);
+
 private:
     ::OpusEncoder *encoder = nullptr;
     int frameSamples = 0;
     int frameChannels = 0;
 };
+
 } // namespace AV
 } // namespace Core
 } // namespace Acheron
