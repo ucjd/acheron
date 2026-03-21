05b59f05d5d7d41740840e666cdb04d591ae5eaa
ucjd <134995775+ucjd@users.noreply.github.com>
2026-03-21 04:48:08 -0500

    Update OpusEncoder.hpp

--- src/Core/AV/OpusEncoder.hpp
+++ src/Core/AV/OpusEncoder.hpp
@@ -16,7 +16,6 @@ public:
     void setBitrate(int bitrate);
     void setComplexity(int complexity);
     void setSignalType(int signalType);
-    void setBandwidth(int bandwidth);
     void setFec(bool enabled);
     void setDtx(bool enabled);
     void setPacketLossPercent(int percent);
