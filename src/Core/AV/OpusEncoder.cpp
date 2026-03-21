3d8d89df950fb16b5e0177ba04595e5c57d769ed
ucjd <134995775+ucjd@users.noreply.github.com>
2026-03-21 01:56:10 -0500

    fr this time (and added Lsb depth control)

--- src/Core/AV/OpusEncoder.cpp
+++ src/Core/AV/OpusEncoder.cpp
@@ -1,58 +1,48 @@
 #include "OpusEncoder.hpp"
 #include "IAudioBackend.hpp"
 #include "Core/Logging.hpp"
-
 namespace Acheron {
 namespace Core {
 namespace AV {
-
 static constexpr int OPUS_MAX_PACKET_SIZE = 4000;
-
 OpusEncoder::OpusEncoder() = default;
-
 OpusEncoder::~OpusEncoder()
 {
     if (encoder)
         opus_encoder_destroy(encoder);
 }
-
 bool OpusEncoder::init(int sampleRate, int channels)
 {
     frameSamples = sampleRate * AUDIO_FRAME_DURATION_MS / 1000;
     frameChannels = channels;
-
     int error;
     encoder = opus_encoder_create(sampleRate, channels, OPUS_APPLICATION_AUDIO, &error);
     if (error != OPUS_OK || !encoder) {
         qCCritical(LogVoice) << "Failed to create Opus encoder:" << opus_strerror(error);
         return false;
     }
-
     setBitrate(OPUS_BITRATE_MAX);
     setComplexity(10);
     setSignalType(OPUS_SIGNAL_MUSIC);
-    setBandwidth(OPUS_BANDWIDTH_FULLBAND);
+    setMaxBandwidth(OPUS_BANDWIDTH_FULLBAND);
+    setLsbDepth(16);
     setVbr(true);
     setVbrConstraint(false);
     setFec(false);
     setDtx(false);
     setPacketLossPercent(0);
-
     return true;
 }
-
 QByteArray OpusEncoder::encode(const QByteArray &pcmFrame)
 {
     if (!encoder)
         return {};
-
     int expectedSize = frameSamples * frameChannels * static_cast<int>(sizeof(opus_int16));
     if (pcmFrame.size() != expectedSize) {
         qCWarning(LogVoice) << "OpusEncoder: unexpected PCM size" << pcmFrame.size()
                             << "expected" << expectedSize;
         return {};
     }
-
     QByteArray encoded(OPUS_MAX_PACKET_SIZE, '\0');
     int bytes = opus_encode(encoder,
                             reinterpret_cast<const opus_int16 *>(pcmFrame.constData()),
@@ -63,65 +53,59 @@ QByteArray OpusEncoder::encode(const QByteArray &pcmFrame)
         qCWarning(LogVoice) << "Opus encode failed:" << opus_strerror(bytes);
         return {};
     }
-
     encoded.resize(bytes);
     return encoded;
 }
-
 void OpusEncoder::setBitrate(int bitrate)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_BITRATE(bitrate));
 }
-
 void OpusEncoder::setComplexity(int complexity)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_COMPLEXITY(complexity));
 }
-
 void OpusEncoder::setSignalType(int signalType)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_SIGNAL(signalType));
 }
-
-void OpusEncoder::setBandwidth(int bandwidth)
+void OpusEncoder::setMaxBandwidth(int bandwidth)
 {
     if (encoder)
-        opus_encoder_ctl(encoder, OPUS_SET_BANDWIDTH(bandwidth));
+        opus_encoder_ctl(encoder, OPUS_SET_MAX_BANDWIDTH(bandwidth));
 }
-
+void OpusEncoder::setLsbDepth(int depth)
+{
+    if (encoder)
+        opus_encoder_ctl(encoder, OPUS_SET_LSB_DEPTH(depth));
+}
 void OpusEncoder::setFec(bool enabled)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_INBAND_FEC(enabled ? 1 : 0));
 }
-
 void OpusEncoder::setDtx(bool enabled)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_DTX(enabled ? 1 : 0));
 }
-
 void OpusEncoder::setPacketLossPercent(int percent)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_PACKET_LOSS_PERC(percent));
 }
-
 void OpusEncoder::setVbr(bool enabled)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_VBR(enabled ? 1 : 0));
 }
-
 void OpusEncoder::setVbrConstraint(bool enabled)
 {
     if (encoder)
         opus_encoder_ctl(encoder, OPUS_SET_VBR_CONSTRAINT(enabled ? 1 : 0));
 }
-
 } // namespace AV
 } // namespace Core
 } // namespace Acheron
